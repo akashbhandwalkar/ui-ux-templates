eaa2a964a333ebc72b543c87fb79bb741b25753f
Akash B <abakash02@gmail.com>
2020-06-23 22:23:56 +0530

    added laptop

---
+++ macbook/app.js
@@ -0,0 +1,8 @@
+window.addEventListener('keydown', (e) => {
+    if(e.keyCode == 13) {
+        document.querySelector('.welcome').style.display = 'block';
+        document.querySelector('.para').style.display = 'block';
+
+        
+    }
+})
