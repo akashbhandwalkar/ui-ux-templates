dff3fc2766828ebd96c46feb67f5aa5421736e94
Akash B <abakash02@gmail.com>
2020-06-23 20:01:10 +0530

    dixed the power

---
+++ fix-the-power/app.js
@@ -0,0 +1,14 @@
+// var tween = gsap.to(".eye", {
+//     duration: 0.3, 
+//     x: 20,
+//     rotation: 360,
+//     repeat: true
+    
+//   });
+//   tween.play();
+  // click handlers for controlling the tween instance...
+//   document.querySelector("#play").onclick = () => tween.play();
+//   document.querySelector("#pause").onclick = () => tween.pause();
+//   document.querySelector("#resume").onclick = () => tween.resume();
+//   document.querySelector("#reverse").onclick = () => tween.reverse();
+//   document.querySelector("#restart").onclick = () => tween.restart();
