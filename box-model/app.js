b846dcd5120ad39ce02224699eca834938d9012d
Akash B <abakash02@gmail.com>
2020-07-06 12:00:05 +0530

    added filter

--- box-model/app.js
+++ box-model/app.js
@@ -1,5 +1,5 @@
 
-
+function paintstars(){
 
 for(var i = 0; i < 200; i++) {
     var element = document.createElement('div');
@@ -9,11 +9,14 @@ for(var i = 0; i < 200; i++) {
     document.querySelector('body').append(element);
 }
 
-for(var i = 0; i < 20; i++) {
+for(var i = 0; i < 10; i++) {
     var element = document.createElement('div');
     element.classList.add('medium-star');
     element.style.top = ((Math.random() * 1000) % 600)  + "px";
     element.style.left = ((Math.random() * 2000) % 1500) + "px";
     document.querySelector('body').append(element);
 }
+
+};
+paintstars();
 
