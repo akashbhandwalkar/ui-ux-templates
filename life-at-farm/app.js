bbb219ddf9f9d1639cf04218bf53dd15894fa5e7
Akash B <abakash02@gmail.com>
2020-12-19 19:30:13 +0530

    added stars, moon, sun and animation

--- life-at-farm/app.js
+++ life-at-farm/app.js
@@ -1,3 +1,19 @@
 const body = document.querySelector('body');
 
 body.style.transform = `scale(${(window.innerWidth / 1600) > 1 ? 1 : (window.innerWidth / 1600)})`
+
+
+for(var i = 0; i < 200; i++) {
+    var element = document.createElement('div');
+    element.classList.add('little-star');
+    element.style.top = ((Math.random() * 1000) % 300)  + "px";
+    element.style.left = ((Math.random() * 1000) % 1000) + "px";
+    document.querySelector('.nature-tree-container').append(element);
+}
+for(var i = 0; i < 200; i++) {
+    var element = document.createElement('div');
+    element.classList.add('little-star');
+    element.style.top = ((Math.random() * 1000) % 300)  + "px";
+    element.style.left = ((Math.random() * 1000) % 1000) + 700 + "px";
+    document.querySelector('.nature-tree-container').append(element);
+}
